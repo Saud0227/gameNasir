1af33f550dc92a3c0ca4dfc8d4a33e76f9e7cf11
Saud0227 <saud02tech@gmail.com>
2021-09-24 16:12:53 +0200

    made speed for big window

--- actor.js
+++ actor.js
@@ -1,20 +1,20 @@
 class actor{
 
-  constructor(x,y,s,_cV,img = null){
+  constructor(x,y,s,si,_cV,pImg){
     this.pos=createVector(x,y);
     this.posDelta=createVector(0,0);
     this.speed=s;
-    this.size=15;
+    this.size=si;
     this.rgb=_cV;
     this.walkTo=[];
-    if(img != null){
-      this.img = img
+    if(pImg != null){
+      this.pImg = pImg
     }
 
   }
 
   update() {
-    if(this.walkTo.length>0&&this.walkTo[0].dist(this.pos)>=2){
+    if(this.walkTo.length>0&&this.walkTo[0].dist(this.pos)>=4){
       let tempV=createVector(this.pos.x,this.pos.y);
       tempV.sub(this.walkTo[0]);
       if(tempV.mag()>2){
@@ -22,7 +22,7 @@ class actor{
         tempV.mult(-this.speed);
       }
       this.posDelta.set(tempV);
-    }else if(this.walkTo.length>0&&this.walkTo[0].dist(this.pos)<2){
+    }else if(this.walkTo.length>0&&this.walkTo[0].dist(this.pos)<4){
       this.walkTo.splice(0,1);
     }
     this.pos.add(this.posDelta);
@@ -30,7 +30,13 @@ class actor{
     fill(this.rgb.x,this.rgb.y,this.rgb.z);
     noStroke();
     rectMode(CENTER);
-    rect(this.pos.x,this.pos.y,this.size,this.size);
+    push();
+    translate(this.pos.x,this.pos.y);
+    rect(0,0,this.size,this.size);
+    if(this.pImg != null){
+      image(this.pImg, -this.size/2, -this.size/2, this.size,this.size);
+    }
+    pop();
   }
 
   addWalkPos(pos){
